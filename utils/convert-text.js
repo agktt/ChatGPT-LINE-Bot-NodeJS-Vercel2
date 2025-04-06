f49951cd164df658a354a11f2b7c64b26101c654
podoro8139 <148435042+podoro8139@users.noreply.github.com>
2025-04-06 11:12:42 +0900

    Update convert-text.js

--- utils/convert-text.js
+++ utils/convert-text.js
@@ -1,10 +1,7 @@
-import * as OpenCC from 'opencc-js';
-import config from '../config/index.js';
+// app/utils/convertText.js
 
 const convertText = (text) => {
-  if (config.APP_LANG !== 'ja') return text;
-  const converter = OpenCC.Converter({ from: 'cn', to: 'tw' });
-  return converter(text);
+  return text; // 変換なしでそのまま返す
 };
 
 export default convertText;

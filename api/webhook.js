9336895d683ae27bfb4ab3af3b90f022e15a1cb3
agktt <101738000+agktt@users.noreply.github.com>
2025-05-02 20:03:14 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    res.status(200).send("Webhook OK（仮テスト）");
+    res.status(200).send("Webhook OK (仮テスト)");
   } else {
     res.status(405).send("Method Not Allowed");
   }

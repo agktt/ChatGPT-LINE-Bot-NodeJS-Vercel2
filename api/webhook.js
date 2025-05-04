ce0e2d92b44ff617eb1fbde322dfa80715ba1c56
agktt <101738000+agktt@users.noreply.github.com>
2025-05-05 00:00:37 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,30 @@
-import { middleware } from '@line/bot-sdk';
+import express from 'express';
+import { middleware, Client } from '@line/bot-sdk';
+
+const app = express();
 
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
-export default middleware(config);
+const client = new Client(config);
+
+app.post('/api/webhook', middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: `あなたのメッセージ: ${event.message.text}`
+  });
+}
+
+export default app;

027fb1b1c08c6a3f2266f2e4b6231b2f79478323
agktt <101738000+agktt@users.noreply.github.com>
2025-05-04 23:57:00 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -5,18 +5,4 @@ const config = {
   channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
-const lineMiddleware = middleware(config);
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed');
-  }
-
-  return new Promise((resolve, reject) => {
-    lineMiddleware(req, res, () => {
-      console.log('署名検証成功');
-      res.status(200).send('OK');
-      resolve();
-    });
-  });
-}
+export default middleware(config);
